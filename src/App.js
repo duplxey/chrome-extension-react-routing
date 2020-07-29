b1434fcf3ae7018df7a13b02f1f2fb83422b5421
duplxey <nick.tomazic@gmail.com>
2020-07-29 17:43:46 +0200

    Separated navigation + added bootstrap.

--- src/App.js
+++ src/App.js
@@ -4,25 +4,14 @@ import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
 import Users from "./pages/users";
 import About from "./pages/about";
 import Home from "./pages/home";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/users">Users</Link>
-              </li>
-            </ul>
-          </nav>
+          <Navigation />
           {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
           <Switch>
