04d54e81c247632ca7489d2eda8be7d081729506
duplxey <nick.tomazic@gmail.com>
2020-07-29 17:54:30 +0200

    Navigation sidebar.

--- src/App.js
+++ src/App.js
@@ -10,22 +10,26 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <div>
-          <Navigation />
-          {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-          <Switch>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/users">
-              <Users />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-3">
+              <Navigation />
+            </div>
+            <div className="col-9">
+              <Switch>
+                <Route path="/about">
+                  <About />
+                </Route>
+                <Route path="/users">
+                  <Users />
+                </Route>
+                <Route path="/">
+                  <Home />
+                </Route>
+              </Switch>
+            </div>
+          </div>
+          </div>
       </Router>
     </div>
   );
